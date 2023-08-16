0abe40ac9831f6af997b27a5a06b2d44481c651d
«Mansur» <sozd.space@gmail.com>
2023-08-16 18:27:51 +0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TodoInput from "./TodoInput";
+
+interface Todo {
+  id: number;
+  title: string;
+  isDone: boolean;
+}
 
 function App() {
+  const [todoArr, setTodoArr] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(e.target.value);
+    console.log(todo);
+  };
+
+  const handleAddTodo = (e: React.FormEvent) => {
+    console.log(todoArr);
+    e.preventDefault();
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: todo,
+      isDone: false,
+    };
+
+    setTodoArr([newTodo, ...todoArr]);
+    setTodo("");
+  };
+
+  const handleDone = (id: number) => {
+    const newArr = todoArr.map((el) => {
+      if (el.id === id) {
+        return { ...el, isDone: !el.isDone };
+      }
+      return el;
+    });
+    setTodoArr(newArr);
+  };
+
+  const handleDelete = (id: number) => {
+    setTodoArr(todoArr.filter((el) => el.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo</h1>
+      <form onSubmit={handleAddTodo} action="">
+        <TodoInput handleChange={handleChange} value={todo} />
+        <button disabled={!todo.trim()}>ADD TODO</button>
+      </form>
+
+      {todoArr.map((el) => {
+        return (
+          <div className={el.isDone ? "todo-done" : "todo"}>
+            <input
+              type="checkbox"
+              checked={el.isDone}
+              onChange={() => handleDone(el.id)}
+            />
+            <span>{el.title}</span>
+            <button onClick={() => handleDelete(el.id)}>X</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/TodoInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface FormProps {
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string | undefined;
+}
+
+const TodoInput = ({ handleChange, value }: FormProps) => {
+  return <input onChange={handleChange} value={value} type="text" />;
+};
+
+export default TodoInput;
